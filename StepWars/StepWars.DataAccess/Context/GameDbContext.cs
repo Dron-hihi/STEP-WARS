2df67990bd6c8fd0733cbd84e4a29cec91b40e4f
Andriy23780 <Andriy23780@gmail.com>
2019-11-09 18:31:04 +0200

    resolve conflict

--- StepWars/StepWars.DataAccess/Context/GameDbContext.cs
+++ StepWars/StepWars.DataAccess/Context/GameDbContext.cs
@@ -1,18 +1,19 @@
 ﻿namespace StepWars.DataAccess.Context
 {
-    using StepWars.DataAccess.Enitites;
+    using StepWars.DataAccess.Entities;
     using System;
     using System.Data.Entity;
     using System.Linq;
 
-    public class GameDbContext : DbContext
+    public class GameDBContext : DbContext
     {
-        public GameDbContext() : base("name=GameDbContext")
+        public GameDBContext() : base("name=GameDBContext")
         {
         }
 
         public DbSet<User> Users { get; set; }
-        public DbSet<Enemy> Enemy { get; set; }
+        public DbSet<Enemy> Enemies { get; set; }
         public DbSet<Kill> Kills { get; set; }
     }
+
 }
