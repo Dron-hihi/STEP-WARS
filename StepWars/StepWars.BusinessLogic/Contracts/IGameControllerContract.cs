9b95f673edae0e34e64886eb7e94a56f6cc3bbee
Bandera <42476974+Bandera1@users.noreply.github.com>
2019-11-09 02:43:22 +0200

    Start to make server.

---
+++ StepWars/StepWars.BusinessLogic/Contracts/IGameControllerContract.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StepWars.BusinessLogic.Contracts
+{
+    // Контракт де буде відбуватися уся ігрова логіка(пересування,стрільба,смерть,бонуси і т.д)
+    public interface IGameControllerContract
+    {
+    }
+}
---
+++ StepWars/StepWars.BusinessLogic/Contracts/IRegistrationContract.cs
@@ -0,0 +1,22 @@
+﻿using StepWars.BusinessLogic.Clasess.Inline;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StepWars.BusinessLogic.Contracts
+{
+    // Контракт, який відповідає за реєстрацію нового гравця
+    public interface IRegistrationContract
+    {
+        // Функція, яка повертає всі кораблі для вибору
+        List<StarShipDTO> GetAllShips();
+        // Функція, яка перевіряє чи вже є гравець з таким самим ніком в грі
+        bool CheckToUserExist(string nickName);
+        // Функція, яка перевіряє чи є в гравця права адміна
+        bool CheckToAdminRules(string nickName);
+        // Функція, яка реєструє нового гравця
+        bool RegisterNewPlayer(string nickName, StarShip startShip);
+    }
+}
