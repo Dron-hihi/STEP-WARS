46d104ca592c9fad922d34b262d1ac5bf35b1d98
Bandera <42476974+Bandera1@users.noreply.github.com>
2019-11-10 17:55:46 +0200

    Added image extension

--- StepWars/StepWars.BusinessLogic/Clasess/DTO/StarShipDTO.cs
+++ StepWars/StepWars.BusinessLogic/Clasess/DTO/StarShipDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,6 +8,6 @@ using System.Threading.Tasks;
 namespace StepWars.BusinessLogic.Clasess.Inline
 {
     public class StarShipDTO
-    {
+    {     
     }
 }
