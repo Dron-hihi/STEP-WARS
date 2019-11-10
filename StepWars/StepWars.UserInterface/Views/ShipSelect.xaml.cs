69899fc9b8b9137717bb6026c62b307d730a6d16
Andriy23780 <Andriy23780@gmail.com>
2019-11-10 17:29:29 +0200

    fixer folders issue

---
+++ StepWars/StepWars.UserInterface/Views/ShipSelect.xaml.cs
@@ -0,0 +1,34 @@
+﻿using MahApps.Metro.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace UserInterface
+{
+    /// <summary>
+    /// Логика взаимодействия для WPF_Selected_Ship.xaml
+    /// </summary>
+    public partial class ShipSelect : MetroWindow
+    {
+        public ShipSelect()
+        {
+            InitializeComponent();
+        }
+
+        private void Battle_Start(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Open Win Forms");
+            this.Close();
+        }
+    }
+}
