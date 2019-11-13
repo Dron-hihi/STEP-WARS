5e3cba64a6d1e63a6ee3f22fc9ba2636475bbf93
Дмитрий <sandora250@gmail.com>
2019-11-13 15:32:21 +0200

    StarShip

---
+++ StepWars/StepWars.DataAccess/Enitites/StarShip.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StepWars.DataAccess.Enitites
+{
+    public class StarShip : BaseEntity
+    {
+        public string Name { get; set; }
+        public int Damage { get; set; }
+        public int Health { get; set; }
+        public int Speed { get; set; }
+        public string Image { get; set; }
+    }
+}
